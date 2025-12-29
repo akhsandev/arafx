8305ae10a9ebdb930f411c93de7ff431f005528a
akhsandev <97935038+akhsandev@users.noreply.github.com>
2025-12-29 18:04:13 +0800

    aswil

--- fluidadinamis/data/wo1/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const iframe = document.getElementById('simulation-iframe');
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-   
-    iframe.width = width;
-    iframe.height = height;
-   });
